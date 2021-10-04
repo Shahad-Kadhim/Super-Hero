78c0bc2c7cba46bb24efbc05a226725ead5f6c52
shahad tariq <shahadtariq9@gmail.com>
2021-10-04 17:41:46 +0300

    add interceptor and state of response

--- app/src/main/java/com/lemon/team/superhero/model/network/API.kt
+++ app/src/main/java/com/lemon/team/superhero/model/network/API.kt
@@ -1,15 +1,19 @@
 package com.lemon.team.superhero.model.network
 
-import com.lemon.team.superhero.model.data.response.SuperHeroResponse
 import com.lemon.team.superhero.util.Constants.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object API {
 
+    private val clientForInterceptor = OkHttpClient.Builder().addInterceptor( HttpLoggingInterceptor()).build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(clientForInterceptor)
         .build()
 
     val apiService = retrofit.create(SuperHeroService::class.java)
--- app/src/main/java/com/lemon/team/superhero/ui/fragment/search/SearchFragment.kt
+++ app/src/main/java/com/lemon/team/superhero/ui/fragment/search/SearchFragment.kt
@@ -1,9 +1,14 @@
 package com.lemon.team.superhero.ui.fragment.search
 
 import android.view.LayoutInflater
+import androidx.lifecycle.lifecycleScope
 import com.lemon.team.superhero.R
 import com.lemon.team.superhero.databinding.FragmentSearchBinding
+import com.lemon.team.superhero.model.Repository
 import com.lemon.team.superhero.ui.fragment.base.BaseFragment
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>() {
