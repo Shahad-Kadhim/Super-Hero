59f5321ec21ceaa909f988e1a472cce275486a6a
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-10-06 04:36:39 +0300

    add search fragment to container fragment view

--- app/src/main/java/com/lemon/team/superhero/ui/MainActivity.kt
+++ app/src/main/java/com/lemon/team/superhero/ui/MainActivity.kt
@@ -2,15 +2,25 @@ package com.lemon.team.superhero.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.lemon.team.superhero.databinding.ActivityMainBinding
+import com.lemon.team.superhero.ui.fragment.search.SearchFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var _binding:ActivityMainBinding
+    private lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(_binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        addFragment(SearchFragment())
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(
+            binding.fragmentContainer.id,
+            fragment
+        ).commit()
     }
 }
