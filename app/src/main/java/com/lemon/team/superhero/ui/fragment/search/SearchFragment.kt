db50bcb82468f9a77802c4deb6b1fa2a61e8b03d
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-10-07 17:18:42 +0300

    add on click item an don click back

--- app/src/main/java/com/lemon/team/superhero/ui/fragment/search/SearchFragment.kt
+++ app/src/main/java/com/lemon/team/superhero/ui/fragment/search/SearchFragment.kt
@@ -3,12 +3,16 @@ package com.lemon.team.superhero.ui.fragment.search
 import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
+import com.lemon.team.superhero.R
 import com.lemon.team.superhero.databinding.FragmentSearchBinding
 import com.lemon.team.superhero.model.reponse.SuperHeroSearchResultResponse
 import com.lemon.team.superhero.ui.IView
 import com.lemon.team.superhero.ui.fragment.SuperHeroInteractionListener
 import com.lemon.team.superhero.ui.fragment.base.BaseFragment
+import com.lemon.team.superhero.util.goToFragment
 import com.lemon.team.superhero.util.hide
 import com.lemon.team.superhero.util.onClickSearch
 import com.lemon.team.superhero.util.show
@@ -33,8 +37,13 @@ class SearchFragment : BaseFragment<FragmentSearchBinding,SearchPresenter>(),IVi
     }
 
     override fun callbacks() {
-        binding?.searchField?.onClickSearch {
-            search(it)
+        binding?.apply {
+            searchField.onClickSearch { query->
+                search(query)
+            }
+            backBtn.setOnClickListener {view ->
+                view.findNavController().popBackStack()
+            }
         }
     }
 
@@ -74,7 +83,7 @@ class SearchFragment : BaseFragment<FragmentSearchBinding,SearchPresenter>(),IVi
 
 
     override fun onClickItem(superHeroId: String) {
-
+        binding?.root?.goToFragment(SearchFragmentDirections.actionSearchFragmentToDetailsFragment(superHeroId))
     }
 
 }
