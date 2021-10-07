131cfef383d8600c62dcf9c8b05cff0e9a025ae6
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-10-07 09:01:34 +0300

    add search view and nav to search fragment

--- app/src/main/java/com/lemon/team/superhero/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/lemon/team/superhero/ui/fragment/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import com.lemon.team.superhero.databinding.FragmentHomeBinding
 import com.lemon.team.superhero.ui.fragment.base.BaseFragment
 import com.lemon.team.superhero.ui.fragment.base.BasePresenter
+import com.lemon.team.superhero.util.goToFragment
 
 class HomeFragment:BaseFragment<FragmentHomeBinding,HomePresenter>() {
     override val LOG_TAG: String="HOME_FRAGMENT"
@@ -16,7 +17,15 @@ class HomeFragment:BaseFragment<FragmentHomeBinding,HomePresenter>() {
     }
 
     override fun callbacks() {
-
+        binding?.searchField?.setOnEditorActionListener { view, _, _ ->
+            if(view.text.isNotBlank()){
+                view.goToFragment(
+                    HomeFragmentDirections.actionHomeFragmentToSearchFragment(view.text.toString())
+                )
+                return@setOnEditorActionListener  true
+            }
+            return@setOnEditorActionListener false
+        }
     }
 
 }
