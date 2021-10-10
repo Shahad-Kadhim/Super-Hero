d3baa7dca3c090402edf2a7d9561bbc9a7b65852
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-10-10 15:33:03 +0300

    add splash screen

--- app/src/main/java/com/lemon/team/superhero/ui/MainActivity.kt
+++ app/src/main/java/com/lemon/team/superhero/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.lemon.team.superhero.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lemon.team.superhero.R
 import com.lemon.team.superhero.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_SuperHero)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
